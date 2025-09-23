9b7245915771cbaf229a69d1055352859dd6a08f
Riya Shukla <missriyashukla@gmail.com>
2025-09-23 18:36:37 +0530

    SelectionSortCode

--- src/SortingAlgo/SelectionSort.java
+++ src/SortingAlgo/SelectionSort.java
@@ -1,8 +1,35 @@
 package SortingAlgo;
 
+
+
 public class SelectionSort {
     public static void main(String[] args)
     {
+        int[] arr = {5,9,4,8,9,7};
+
+
+
+        int n = arr.length;
+        for(int i =0; i<n-1; i++){
+            int min = Integer.MAX_VALUE;
+            int minidx =-1;
+            for(int j =i; j<n ;j++){ // pure array me har pass me
+                //minimum element find karke
+                //index assign karna.
+                if(arr[j]<min){
+                    min = arr[j];
+                    minidx = j;
+
+                }
+            }
+            int temp = arr[i];
+            arr[i]= arr[minidx]; // then swap that index with ith index.
+            arr[minidx] = temp;
+        }
+        for(int x:arr)
+        System.out.print(x+"  ");
 
     }
 }
+
+
