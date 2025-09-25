eb740fafe19ba62c9fc9101624f275a71c029a38
Riya Shukla <missriyashukla@gmail.com>
2025-09-25 14:35:14 +0530

    Kthsmallest commit 2

--- src/SortingAlgo/KthSmallest.java
+++ src/SortingAlgo/KthSmallest.java
@@ -1,8 +1,9 @@
 package SortingAlgo;
 
+// Find the kth smallest element in an array.
 public class KthSmallest {
     public static void main(String[] args){
-        int[] arr ={ 4 ,7,9,7,3,2};
+        int[] arr ={ 4 ,7,9,3,2};
         int k =4;
         for(int i=0;i<k;i++){
             int min = Integer.MAX_VALUE;
