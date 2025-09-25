e0abe336504edf2a7c1cc2f586710ce8074abd3c
Riya Shukla <missriyashukla@gmail.com>
2025-09-25 14:24:51 +0530

    Kthsmallest

---
+++ src/SortingAlgo/KthSmallest.java
@@ -0,0 +1,25 @@
+package SortingAlgo;
+
+public class KthSmallest {
+    public static void main(String[] args){
+        int[] arr ={ 4 ,7,9,7,3,2};
+        int k =4;
+        for(int i=0;i<k;i++){
+            int min = Integer.MAX_VALUE;
+            int minidx = -1;
+            for (int j = i; j < arr.length; j++) {
+                if(arr[j]<min){
+                    min = arr[j];
+                    minidx = j;
+                }
+
+            }
+            int temp = arr[i];
+            arr[i]=arr[minidx];
+            arr[minidx]=temp;
+        }
+        System.out.println(arr[k-1]);
+
+
+    }
+}
